12653c1159c88778185544a30df5dcadca26a5f0
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-05-18 17:57:43 +0100

    update

--- src/functions/creating-functions-multiple-args.js
+++ src/functions/creating-functions-multiple-args.js
@@ -10,6 +10,18 @@
 //
 // TODO: write code below
 
+
+function addNum(lowerNum, upperNum) {
+   const betweenNum=[]
+   for (let i=lowerNum; i<=upperNum; i++)
+   betweenNum.push (i)
+   return betweenNum
+  
+}
+console.log (addNum(1,3))
+console.log (addNum(10,13))
+console.log(addNum(-1,1))
+
 // define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
 // marks appended to the end. The number of exclamation marks should be
@@ -22,8 +34,13 @@
 //
 // TODO: write code below
 
+function upperCase (letter,number) {
+  
+
+}
+
 // change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined // change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  a: addNum, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: upperCase // change undefined to be the name of the function defined to return the string with exclamations (the second todo)
 }
--- src/functions/creating-functions.js
+++ src/functions/creating-functions.js
@@ -8,6 +8,13 @@
 //
 // TODO: write code below
 
+function incrementNumber (num) {
+  return num +1
+
+}
+console.log(incrementNumber(1))
+console.log(incrementNumber(2))
+
 // Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
 //
@@ -19,8 +26,18 @@
 //
 // TODO: write code below
 
+function sayHi(name) {
+  newName = name[0].toUpperCase() + name.slice(1)
+  return 'Hi, '+ newName + ' :)'
+}
+
+console.log(sayHi('edward'))
+console.log(sayHi('Aiyana'))
+
+
+
 // TODO: change undefined to be the name of the functions you defined
 module.exports = {
-  a: undefined, // change undefined to be the name of the function you defined to increment a number (the first TODO)
-  b: undefined // change undefined to be the name of the function you defined to say hi (the second TODO)
+  a: incrementNumber, // change undefined to be the name of the function you defined to increment a number (the first TODO)
+  b: sayHi // change undefined to be the name of the function you defined to say hi (the second TODO)
 }
